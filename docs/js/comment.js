62c8eba03469903116944545024a73905b355a6d
Jill Ye <jill.ye@perkinelmer.com>
2018-04-26 12:22:58 +0800

    CleanUp

--- docs/js/comment.js
+++ docs/js/comment.js
@@ -11,9 +11,10 @@ Comments = {
     CLIENT_ID: undefined,
     CLIENT_SECRET: undefined,
     ACCEPT_JSON: "application/json",
-    ACCEPTS : "application/vnd.github.v3+json",
     SCOPE: "public_repo",
     ACCESS_TOKEN: undefined,
+    SESSION_CODE_NAME : 'GIT_CODE',
+    SESSION_ACCESS_TOKEN_NAME: 'GIT_ACCESS_TOKEN',
     CORS_ANYWHERE: 'https://cors-anywhere.herokuapp.com/',
     init: function(owner, repository, clientId, clientSecret) {
         Comments.REPOS = repository;
@@ -23,12 +24,12 @@ Comments = {
         Comments.initAccessToken();
     },
     initAccessToken: function() {
-        var accessToken = window.sessionStorage.getItem("GIT_ACCESS_TOKEN");
+        var accessToken = window.sessionStorage.getItem(Comments.SESSION_ACCESS_TOKEN_NAME);
         if (accessToken) {
             Comments.ACCESS_TOKEN = accessToken;
         } else {
             var url = new URL(window.location.href);
-            var code = url.searchParams.get("code");
+            var code = url.searchParams.get(Comments.SESSION_CODE_NAME);
             if (code) {
                 Comments.getAccessToken(code);
             }
@@ -41,17 +42,15 @@ Comments = {
             client_id:Comments.CLIENT_ID,
             client_secret:Comments.CLIENT_SECRET
         };
-        var querystring = encodeQueryData(data);
-        location.href = 'https://github.com/login/oauth/authorize?' + querystring;
+        location.href = 'https://github.com/login/oauth/authorize?' + $.param(data);
     },
     getAccessToken: function(code) {
        $.ajax({
            method: 'POST',
            url: Comments.CORS_ANYWHERE + 'https://github.com/login/oauth/access_token',
-           headers: {
-               Accept: 'application/json',
-               'X-Requested-With': 'XMLHttpRequest'
-           },
+           accepts: {
+                json: Comments.ACCEPT_JSON
+            },
            data: {
                'client_id':Comments.CLIENT_ID,
                'client_secret':Comments.CLIENT_SECRET,
@@ -60,7 +59,7 @@ Comments = {
        }).done(function(data) {
            if(data.access_token) {
                Comments.ACCESS_TOKEN = data.access_token;
-               window.sessionStorage.setItem("GIT_ACCESS_TOKEN", data.access_token);
+               window.sessionStorage.setItem(Comments.SESSION_ACCESS_TOKEN_NAME, data.access_token);
            }
        })   
     },
@@ -77,12 +76,8 @@ Comments = {
     },
     add: function(issueId, commentText, callback) {
         $.ajax({
-            method: "POST",
-            url: "https://api.github.com/repos/" + Comments.OWNER + "/" + Comments.REPOS + "/issues/" + issueId + "/comments?access_token=" + Comments.ACCESS_TOKEN,
-            headers: {
-               Accept: 'application/json',
-               'X-Requested-With': 'XMLHttpRequest'
-            },
+            method: 'POST',
+            url: "https://api.github.com/repos/" + Comments.OWNER + "/" + Comments.REPOS + "/issues/" + issueId + "/comments?" + $.param({'access_token':Comments.ACCESS_TOKEN}),
             accepts: {
                 json: Comments.ACCEPT_JSON
             },
@@ -96,7 +91,7 @@ Comments = {
     },
     getUser: function(callback){
         $.ajax({
-            url: "https://api.github.com/user?access_token=" + Comments.ACCESS_TOKEN,
+            url: "https://api.github.com/user?" + $.param({'access_token':Comments.ACCESS_TOKEN}),,
             accepts: {
                 json: Comments.ACCEPT_JSON
             },
